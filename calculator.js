cd6c05a5fce68f349047cea1e353aaf316c0aab9
Pavel Ignatiev <pavel@ignatiev.de>
2019-07-07 19:02:50 +0200

    Calculator and validation messages fully functional

--- calculator.js
+++ calculator.js
@@ -1,46 +1,97 @@
-// use session storage if supported, usual JS object otherwise
+// use session storage if supported, an usual JS object otherwise
 let storage = (sessionStorage) ? sessionStorage : {};
 
-const calculate = () => {
-    let totalAmount = Number(storage['initialAmount']);
-    const investmentDuration = Number(storage['investmentDuration']);
-    const returnRate = Number(storage['returnRate']);
-    const inflation = Number(storage['inflation']);
-    const additionalContribution = Number(storage['additionalContribution']);
-
-    for (let i = 1; i <= investmentDuration; i++) {
-        // assume doing an additional contribution before yielding yearly return and calculating inflation
-        totalAmount += additionalContribution;
-        totalAmount += totalAmount * returnRate / 100.0;
-        totalAmount -= totalAmount * inflation / 100.0;
-    }
-
-    return totalAmount;
+// reduce digits nuber after decimal point and append currency symbol
+const formatValue = (value) => {
+    return `${value.toFixed(2)} €`;
 };
 
-$(document).ready(() => {
+// calculate future investment value based on user input
+const calculateFutureValue = function () {
+    // load values from storage
+    const investmentDuration = Number(storage.investmentDuration) || 0;
+    const interestRate = Number(storage.interestRate) || 0;
+    const inflation = Number(storage.inflation) || 0;
+    const additionalContribution = Number(storage.additionalContribution) || 0;
+
+    let futureValue = Number(storage.initialValue);
+
+    // the loop representation is more readable for a layperson
+    // the usual future value formula can be used here too of course
+    // futureValue = initialValue * Math.pow(1 + interestRate, investmentDuration) + 
+    //               + additionalContribution * (1 + interestRate) * (Math.pow(1 + interestRate, investmentDuration) - 1) / interestRate
+    for (let i = 1; i <= investmentDuration; i++) {
+        // calculate interest
+        futureValue += futureValue * interestRate / 100.0;
+        // take inflation into account
+        futureValue -= futureValue * inflation / 100.0;
+        // additional contribution is made at the end of the year
+        futureValue += additionalContribution;
+    }
+
+    storage.futureValue = futureValue;
+
+    return futureValue;
+};
+
+const getFormattedFutureValue = () => formatValue(calculateFutureValue());
+
+const toggleErrorMsg = (msgContainer, currentInput, fields) => {
+    // this is a hack to ensure that the error message is shown
+    // even if the invalid input has lost focus
+    // of course there are better ways to handle this problem
+    let allFieldsValid = true;
+    for (let key in fields) {
+        if (!fields[key].checkValidity()) {
+            allFieldsValid = false;
+        }
+    }
+    if (!currentInput.checkValidity()) {
+        msgContainer.classList.remove('invisible');
+        msgContainer.innerHTML = currentInput.validationMessage;
+    } else {
+        if (allFieldsValid) {
+            msgContainer.classList.add('invisible');
+            msgContainer.innerHTML = '';
+        }
+    }
+};
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    // form field representation
     const fields = {
-        targetAmount: $('#target-amount'),
-        initialAmount: $('#initial-amount'),
-        investmentDuration: $('#investment-duration'),
-        returnRate: $('#return-rate'),
-        additionalContribution: $('#additional-contribution'),
-        inflation: $('#inflation'),
+        initialValue: document.getElementById('initial-value'),
+        investmentDuration: document.getElementById('investment-duration'),
+        interestRate: document.getElementById('interest-rate'),
+        additionalContribution: document.getElementById('additional-contribution'),
+        inflation: document.getElementById('inflation'),
     };
+
+    const futureValue = document.getElementById('future-value');
+    const errorMsg = document.getElementById('error-msg');
 
     for (let inputName in fields) {
         const currentInput = fields[inputName];
 
-        // load default values
-        storage[inputName] = currentInput.val();
+        if (storage[inputName]) {
+            // show existing values from session storage
+            currentInput.value = storage[inputName];
+        } else {
+            // form contains default values, save them into storage
+            storage[inputName] = currentInput.value;
+        }
 
-        // define listeners to recalculate results on user input
-        currentInput.keyup(() => {
-            storage[inputName] = currentInput.val();
-            $('#total-value').text(calculate());
-        });
-    };
+        // listen to user input (manual or up/down button clicks)
+        currentInput.oninput = function () {
+            // store the new input
+            storage[inputName] = currentInput.value;
+            // show recalculated output
+            futureValue.innerHTML = getFormattedFutureValue();
+            // show error message on invalid input
+            toggleErrorMsg(errorMsg, currentInput, fields);
+        };
+    }
 
-    // calculate default total 
-    $('#total-value').text(calculate());
+    // calculate future value from default input values 
+    futureValue.innerHTML = getFormattedFutureValue();
 });
